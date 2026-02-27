e2dfc154e8cc363eedf7a53d358474c729d78e1d
rsh <randrianirinahernandezstephan@gmail.com>
2026-02-27 13:47:53 +0300

    feat: migrate the database with just the table used for the authentication only

---
+++ prisma/migrations/20260227104628_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "auth";
+
+-- CreateTable
+CREATE TABLE "auth"."users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "auth"."users"("email");
